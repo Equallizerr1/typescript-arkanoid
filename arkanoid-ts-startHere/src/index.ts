c5292b1d9869c86515b90de1fbddcedf831add3f
Equallizerr1 <samharradine1994@gmail.com>
2024-08-13 13:11:46 +0100

    paddle now draws to canvas and moves

--- arkanoid-ts-startHere/src/index.ts
+++ arkanoid-ts-startHere/src/index.ts
@@ -7,15 +7,15 @@ import PADDLE_IMAGE from "./images/paddle.png";
 import BALL_IMAGE from "./images/ball.png";
 // Level and colours
 import {
-    PADDLE_SPEED,
-    PADDLE_WIDTH,
-    PADDLE_HEIGHT,
-    PADDLE_STARTX,
-    BALL_SPEED,
-    BALL_SIZE,
-    BALL_STARTX,
-    BALL_STARTY
-} from "./setup"
+	PADDLE_SPEED,
+	PADDLE_WIDTH,
+	PADDLE_HEIGHT,
+	PADDLE_STARTX,
+	BALL_SPEED,
+	BALL_SIZE,
+	BALL_STARTX,
+	BALL_STARTY,
+} from "./setup";
 // Helpers
 import { createBricks } from "./helpers";
 
@@ -23,38 +23,57 @@ let gameOver = false;
 let score = 0;
 
 function setGameOver(view: CanvasView) {
-    view.drawInfo('Game Over!');
-    gameOver = false;
+	view.drawInfo("Game Over!");
+	gameOver = false;
 }
 
 function setGameWin(view: CanvasView) {
-    view.drawInfo('Game Won!');
-    gameOver = false;
+	view.drawInfo("Game Won!");
+	gameOver = false;
 }
 
 function gameLoop(
-    view: CanvasView,
-    bricks: Brick[],
-    // paddle: Paddle,
-    // ball: Ball
-) { 
-    view.clear();
-    view.drawBricks(bricks);
+	view: CanvasView,
+	bricks: Brick[],
+	paddle: Paddle
+	// ball: Ball
+) {
+	view.clear();
+	view.drawBricks(bricks);
+	view.drawSprite(paddle);
 
-    requestAnimationFrame(() => gameLoop(view, bricks))
+	// Move paddle and check so it wont exit the playfield
+	if (
+		(paddle.isMovingLeft && paddle.pos.x > 0) ||
+		(paddle.isMovingRight && paddle.pos.x < view.canvas.width - paddle.width)
+	) {
+		paddle.movePaddle();
+	}
+	requestAnimationFrame(() => gameLoop(view, bricks, paddle));
 }
 
-function startGame(view: CanvasView) { 
-    // Reset display
-    score = 0;
-    view.drawInfo('');
-    view.drawScore(0);
-    // Create all bricks
-    const bricks = createBricks();
+function startGame(view: CanvasView) {
+	// Reset display
+	score = 0;
+	view.drawInfo("");
+	view.drawScore(0);
+	// Create all bricks
+	const bricks = createBricks();
+	// Create paddle
+	const paddle = new Paddle(
+		PADDLE_SPEED,
+		PADDLE_WIDTH,
+		PADDLE_HEIGHT,
+		{
+			x: PADDLE_STARTX,
+			y: view.canvas.height - PADDLE_HEIGHT - 5,
+		},
+		PADDLE_IMAGE
+	);
 
-    gameLoop(view, bricks)
+	gameLoop(view, bricks, paddle);
 }
 
 // Create a new view
-const view = new CanvasView('#playField')
+const view = new CanvasView("#playField");
 view.initStartButton(startGame);
